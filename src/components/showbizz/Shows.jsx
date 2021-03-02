4516665f487ee120640679b49184e858450c425c
Nemanja Spasojevic <nemanja.spasojevic.memi@gmail.com>
2021-03-02 14:58:03 +0100

    Color changes

--- src/components/showbizz/Shows.jsx
+++ src/components/showbizz/Shows.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import showpic from "../../assets/images/night-4371499_1920.jpg";
+import showspic from "../../assets/images/night-4371499_1920.jpg";
 
 export const Shows = () => {
     return (
-        <section className="show">
-            <div className="showItems">
-            <div className="showItemsImage">
-            <img src={showpic} alt="showpic img" className="showItem" />
+        <section className="shows">
+            <div className="showsItems">
+            <div className="showsItemsImage">
+            <img src={showspic} alt="showspic img" className="showsItem" />
             </div>
-            <h5 className="showItemTitle" >Emisija</h5>
+            <h5 className="showsItemTitle" >Emisija</h5>
             </div>
-            <div className="showItems">
-            <div className="showItemsImage">
-            <img src={showpic} alt="showpic img" className="showItem" />
+            <div className="showsItems">
+            <div className="showsItemsImage">
+            <img src={showspic} alt="showspic img" className="showsItem" />
             </div>
-            <h5 className="showItemTitle" >Emisija</h5>
+            <h5 className="showsItemTitle" >Emisija</h5>
             </div>
-            <div className="showItems">
-            <div className="showItemsImage">
-            <img src={showpic} alt="showpic img" className="showItem" />
+            <div className="showsItems">
+            <div className="showsItemsImage">
+            <img src={showspic} alt="showspic img" className="showsItem" />
             </div>
-            <h5 className="showItemTitle" >Emisija</h5>
+            <h5 className="showsItemTitle" >Emisija</h5>
             </div>
         </section>
     )
