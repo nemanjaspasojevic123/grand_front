522817b72ac785a55dde899ae2b3105748472739
Nemanja Spasojevic <nemanja.spasojevic.memi@gmail.com>
2021-03-04 00:20:57 +0100

    News for Showbizz section

--- src/components/showbizz/News.jsx
+++ src/components/showbizz/News.jsx
@@ -1,36 +1,60 @@
-import React from 'react';
+import React from "react";
 import showbizzImg1 from "../../assets/images/de22b6bc325b4cc3593b7cf7252bf0c156c2b28e.jpg";
 import showbizzImg2 from "../../assets/images/c3b7d134fc9268bee50baab49ec1f2c07b2e2a65.jpg";
 import showbizzImg3 from "../../assets/images/9ee9aae3a19b81f777a5447e81cda96b266999f7.jpg";
 import showbizzImg4 from "../../assets/images/5e457019b10d0855da8ec84e2c61d71d49a10484.jpg";
 
 export const News = () => {
-    return (
-        <section className="showbizz-news">
-            <div className="showbizz-newsItems">
-            <div className="showbizz-newsItemsImage">
-            <img src={showbizzImg1} alt="showbizz img1" className="showbizz-newsItem" />
-            </div>
-            <h5 className="showbizz-newsItemTitle" >Nove vesti</h5>
-            </div>
-            <div className="showbizz-newsItems">
-            <div className="showbizz-newsItemsImage">
-            <img src={showbizzImg2} alt="showbizz img2" className="showbizz-newsItem" />
-            </div>
-            <h5 className="showbizz-newsItemTitle" >Nove vesti</h5>
-            </div>
-            <div className="showbizz-newsItems">
-            <div className="showbizz-newsItemsImage">
-            <img src={showbizzImg3} alt="showbizz img3" className="showbizz-newsItem" />
-            </div>
-            <h5 className="showbizz-newsItemTitle" >Nove vesti</h5>
-            </div>
-            <div className="showbizz-newsItems">
-            <div className="showbizz-newsItemsImage">
-            <img src={showbizzImg4} alt="showbizz img4" className="showbizz-newsItem" />
-            </div>
-            <h5 className="showbizz-newsItemTitle" >Nove vesti</h5>
-            </div>
-        </section>
-    )
-}
+  return (
+    <section className="showbizz-news">
+      <div className="showbizz-newsItems">
+        <div className="showbizz-newsItemsImage">
+          <img
+            src={showbizzImg1}
+            alt="showbizz img1"
+            className="showbizz-newsItem"
+          />
+        </div>
+        <div className="showbizz-newsTitle">
+            <span className="showbizz-newsItemTitle">Znamo ga kao markantnog, ali nekada je furao TOTALNO DRUGI STIL! Pogledajte kako je Mare izgledao ranije</span>
+        </div>
+      </div>
+      <div className="showbizz-newsItems">
+        <div className="showbizz-newsItemsImage">
+          <img
+            src={showbizzImg2}
+            alt="showbizz img2"
+            className="showbizz-newsItem"
+          />
+        </div>
+        <div className="showbizz-newsTitle">
+            <span className="showbizz-newsItemTitle">Zoran Pejić Peja otvoreno o braku: "Žena mi UVEK OBUVA ČARAPE!"</span>
+        </div>
+      </div>
+      <div className="showbizz-newsItems">
+        <div className="showbizz-newsItemsImage">
+          <img
+            src={showbizzImg3}
+            alt="showbizz img3"
+            className="showbizz-newsItem"
+          />
+        </div>
+        <div className="showbizz-newsTitle">
+            <span className="showbizz-newsItemTitle">PONOVO NA NOGAMA! Amadeus bend se DIŽE IZ PEPELA i nastavlja u novom sastavu!</span>
+        </div>
+      </div>
+      <div className="showbizz-newsItems">
+        <div className="showbizz-newsItemsImage">
+          <img
+            src={showbizzImg4}
+            alt="showbizz img4"
+            className="showbizz-newsItem"
+          />
+        </div>
+        <div className="showbizz-newsTitle">
+            <span className="showbizz-newsItemTitle">Pevač godinu dana živi u KOLIBI, završio u ZATVORU i zakukao „Nadam se da ću uskoro doći u Srbiju“</span>
+        </div>
+      </div>
+    </section>
+  );
+};
--- src/components/showbizz/Poll.jsx
+++ src/components/showbizz/Poll.jsx
@@ -4,60 +4,62 @@ import pollImg1 from "../../assets/images/e20a7ff703ca1fe92c18bab2bbf6c1bbd91c3d
 export const Poll = () => {
   return (
     <div className="showbizz-poll">
-        <div className="showbizz-pollInner">
-      <img src={pollImg1} alt="poll img" className="showbizz-pollItem"></img>
-      <div className="showbizz-pollItemLayer">
-      <h3 >Glasajte za vašeg favorita</h3>
-        <div className="form-check">
-          <input
-            className="form-check-input"
-            type="radio"
-            name="exampleRadios"
-            id="exampleRadios1"
-            value="option1"
-          />
-          <label className="form-check-label" htmlFor="exampleRadios1">
-            Dragan Marković Tajze
-          </label> 
+      <div className="showbizz-pollInner">
+        <img src={pollImg1} alt="poll img" className="showbizz-pollItem"></img>
+        <div className="showbizz-pollItemLayer">
+          <h3>Glasajte za vašeg favorita</h3>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="exampleRadios"
+              id="exampleRadios1"
+              value="option1"
+            />
+            <label className="form-check-label" htmlFor="exampleRadios1">
+              Dragan Marković Tajze
+            </label>
+          </div>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="exampleRadios"
+              id="exampleRadios1"
+              value="option1"
+            />
+            <label className="form-check-label" htmlFor="exampleRadios1">
+              Suzana Smajić
+            </label>
+          </div>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="exampleRadios"
+              id="exampleRadios1"
+              value="option1"
+            />
+            <label className="form-check-label" htmlFor="exampleRadios1">
+              Bato Vuješević
+            </label>
+          </div>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="exampleRadios"
+              id="exampleRadios1"
+              value="option1"
+            />
+            <label className="form-check-label" htmlFor="exampleRadios1">
+              Goran Cvetković
+            </label>
+          </div>
+          <button type="button" className="btn btn-light btn-poll">
+            Glasaj
+          </button>
         </div>
-        <div className="form-check">
-          <input
-            className="form-check-input"
-            type="radio"
-            name="exampleRadios"
-            id="exampleRadios1"
-            value="option1"
-          />
-          <label className="form-check-label" htmlFor="exampleRadios1">
-            Suzana Smajić
-          </label> 
-        </div>
-        <div className="form-check">
-          <input
-            className="form-check-input"
-            type="radio"
-            name="exampleRadios"
-            id="exampleRadios1"
-            value="option1"
-          />
-          <label className="form-check-label" htmlFor="exampleRadios1">
-            Bato Vuješević
-          </label> 
-        </div>
-        <div className="form-check">
-          <input
-            className="form-check-input"
-            type="radio"
-            name="exampleRadios"
-            id="exampleRadios1"
-            value="option1"
-          />
-          <label className="form-check-label" htmlFor="exampleRadios1">
-            Goran Cvetković
-          </label> 
-        </div>
-        <button type="button" class="btn btn-light btn-poll">Glasaj</button>
-      </div>
       </div>
     </div>
   );
