829e6f2fd116328c06db62eb12710f717d3828d8
Nemanja Spasojevic <nemanja.spasojevic.memi@gmail.com>
2021-03-09 15:22:52 +0100

    Nav changes

--- src/components/news-test-page/NewsTest.jsx
+++ src/components/news-test-page/NewsTest.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import banner_left from "../../assets/images/sanja_levo.jpg";
-import banner_right from "../../assets/images/voja_desno.jpg";
 import newsTestImg1 from "../../assets/images/de22b6bc325b4cc3593b7cf7252bf0c156c2b28e.jpg";
+import newsTestImg2 from "../../assets/images/36df36ef682951d0fa8ee0610d1f74490072178a.jpg";
 import { NavBar } from "../navbar/NavBar";
 import { NavTrack } from "../navbar/NavTrack";
+import { PopularNews } from "../showbizz/PopularNews";
 
 export const NewsTest = () => {
   return (
@@ -13,24 +13,42 @@ export const NewsTest = () => {
         <NavTrack />
       </div>
       <div className="newsTest-content">
-        <div className="newsTest-leftBanner">
-          <img
-            src={banner_left}
-            alt="banner_left"
-            className="recepti-leftBannerImage"
-          ></img>
-        </div>
         <div className="newsTest-contentCenter">
-          <div className="newsTest-newsTitle">
-            <h4 className="newsTest-newsItemTitle">
-              Znamo ga kao markantnog, ali nekada je furao TOTALNO DRUGI STIL!
-              Pogledajte kako je Mare izgledao ranije
-            </h4>
-          </div>
           <div className="newsTest-newsItemsImage">
             <img
               src={newsTestImg1}
               alt="newsTest img1"
+              className="newsTest-newsItem"
+            />
+          </div>
+          <div className="newsTest-newsTitle">
+            <h1 className="newsTest-newsItemTitle">
+              Znamo ga kao markantnog, ali nekada je furao TOTALNO DRUGI STIL!
+              Pogledajte kako je Mare izgledao ranije
+            </h1>
+          </div>
+          <p className="newsTest-newsText">
+            Saša Milošević Mare, čuveni je kompozitor i producent, a poslednjih
+            par godina gledamo ga u ulozi stalnog člana žirija popularnog
+            takmičenja "Nikad nije kasno". Iz emisije u emisiju navikao nas je
+            na svoj besprekoran stajling, ali malo ko se seća da je Mare nekada
+            "furao" malo opušteniji stil. Na slici koja datira bar deceniju
+            unazad, poznatog producenta možemo videti sa dugom kosom i, reklo bi
+            se, neukrotivim loknama.
+          </p>
+          <p className="newsTest-newsText">
+            Saša Milošević Mare, čuveni je kompozitor i producent, a poslednjih
+            par godina gledamo ga u ulozi stalnog člana žirija popularnog
+            takmičenja "Nikad nije kasno". Iz emisije u emisiju navikao nas je
+            na svoj besprekoran stajling, ali malo ko se seća da je Mare nekada
+            "furao" malo opušteniji stil. Na slici koja datira bar deceniju
+            unazad, poznatog producenta možemo videti sa dugom kosom i, reklo bi
+            se, neukrotivim loknama.
+          </p>
+          <div className="newsTest-newsItemsImage">
+            <img
+              src={newsTestImg2}
+              alt="newsTest img2"
               className="newsTest-newsItem"
             />
           </div>
@@ -43,31 +61,9 @@ export const NewsTest = () => {
             unazad, poznatog producenta možemo videti sa dugom kosom i, reklo bi
             se, neukrotivim loknama.
           </p>
-          <p className="newsTest-newsText">
-            Saša Milošević Mare, čuveni je kompozitor i producent, a poslednjih
-            par godina gledamo ga u ulozi stalnog člana žirija popularnog
-            takmičenja "Nikad nije kasno". Iz emisije u emisiju navikao nas je
-            na svoj besprekoran stajling, ali malo ko se seća da je Mare nekada
-            "furao" malo opušteniji stil. Na slici koja datira bar deceniju
-            unazad, poznatog producenta možemo videti sa dugom kosom i, reklo bi
-            se, neukrotivim loknama.
-          </p>
-          <p className="newsTest-newsText">
-            Saša Milošević Mare, čuveni je kompozitor i producent, a poslednjih
-            par godina gledamo ga u ulozi stalnog člana žirija popularnog
-            takmičenja "Nikad nije kasno". Iz emisije u emisiju navikao nas je
-            na svoj besprekoran stajling, ali malo ko se seća da je Mare nekada
-            "furao" malo opušteniji stil. Na slici koja datira bar deceniju
-            unazad, poznatog producenta možemo videti sa dugom kosom i, reklo bi
-            se, neukrotivim loknama.
-          </p>
-        </div>
-        <div className="newsTest-rightBanner">
-          <img
-            src={banner_right}
-            alt="banner_right"
-            className="recepti-rightBannerImage"
-          ></img>
+          <div>
+            <PopularNews />
+          </div>
         </div>
       </div>
     </div>
---
+++ src/data/news.js
@@ -0,0 +1,33 @@
+import newsImg1 from "../assets/images/de22b6bc325b4cc3593b7cf7252bf0c156c2b28e.jpg";
+import newsImg2 from "../assets/images/c3b7d134fc9268bee50baab49ec1f2c07b2e2a65.jpg";
+import newsImg3 from "../assets/images/9ee9aae3a19b81f777a5447e81cda96b266999f7.jpg";
+import newsImg4 from "../assets/images/5e457019b10d0855da8ec84e2c61d71d49a10484.jpg";
+
+let news = [
+    {
+        news_id: 1,
+        news_brand: "ZVEZDE GRANDA",
+        news_img: newsImg1,
+        news_title: "Znamo ga kao markantnog, ali nekada je furao TOTALNO DRUGI STIL! Pogledajte kako je Mare izgledao ranije"
+    },
+    {
+        news_id: 2,
+        news_brand: "ZVEZDE GRANDA",
+        news_img: newsImg2,
+        news_title: "Zoran Pejić Peja otvoreno o braku: „Žena mi UVEK OBUVA ČARAPE!“"
+    },
+    {
+        news_id: 3,
+        news_brand: "ZVEZDE GRANDA",
+        news_img: newsImg3,
+        news_title: "PONOVO NA NOGAMA! Amadeus bend se DIŽE IZ PEPELA i nastavlja u novom sastavu!"
+    },
+    {
+        news_id: 4,
+        news_brand: "ZVEZDE GRANDA",
+        news_img: newsImg4,
+        news_title: "Pevač godinu dana živi u KOLIBI, završio u ZATVORU i zakukao „Nadam se da ću uskoro doći u Srbiju“"
+    },
+]
+
+export default news;
