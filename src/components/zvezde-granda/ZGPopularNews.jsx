e1e3128b7d7eaf5009d100944cc3122850f7ecc6
Nemanja Spasojevic <nemanja.spasojevic.memi@gmail.com>
2021-03-16 15:10:53 +0100

    Pagination for Zvezde Granda

--- src/components/zvezde-granda/ZGPopularNews.jsx
+++ src/components/zvezde-granda/ZGPopularNews.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 import popularNews from "../../data/popularNews"
 import { ZGOnePopularNews } from "./ZGOnePopularNews";
+import Pagination from 'react-bootstrap-4-pagination';
 
 export const ZGPopularNews = () => {
+
+  let paginationConfig = {
+    totalPages: 7,
+    currentPage: 3,
+    showMax: 5,
+    size: "sm",
+    activeBorderColor: 'black',
+    activeBgColor: 'grey',
+    color: 'grey',
+    onClick: function (page) {
+       console.log(page);
+     }
+  };
+
   return (
       <section className="zvezdeGranda-popularNews">
         <div className="zvezdeGranda-popularNewsItems">
           {popularNews.map(el => {return <ZGOnePopularNews key={el.news_id} oneNews={el}/>}).slice(0, 10)}
         </div>
+        <Pagination {...paginationConfig} />
       </section>
   );
 };
