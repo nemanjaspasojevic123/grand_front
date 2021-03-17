242431daa88697d0e2e085bc40fce464a0c82d22
Nemanja Spasojevic <nemanja.spasojevic.memi@gmail.com>
2021-03-17 13:24:06 +0100

    Style change for PopularNews

--- src/data/popularNews.js
+++ src/data/popularNews.js
@@ -1,15 +1,15 @@
-import newsImg1 from "../assets/images/639331e65dd407f789da10853f1ea3514f380fb3.jpg";
+import newsImg1 from "../assets/images/d7e73d71699f443a35d3fa3a8b25cd3cc8fcba87.jpg";
 import newsImg2 from "../assets/images/4d5c993bf37765a0ab1edeeee8b7e6fbf20a6b1f.jpg";
-import newsImg3 from "../assets/images/m_abd2e4014d4be6d0e5b239b773bf0f2a37e95855.jpg";
-import newsImg4 from "../assets/images/fb322de76e4e0857b5db00c602013611bb0b13b7.jpg";
-import newsImg5 from "../assets/images/d7e73d71699f443a35d3fa3a8b25cd3cc8fcba87.jpg";
+import newsImg3 from "../assets/images/fb322de76e4e0857b5db00c602013611bb0b13b7.jpg";
+import newsImg4 from "../assets/images/m_abd2e4014d4be6d0e5b239b773bf0f2a37e95855.jpg";
+import newsImg5 from "../assets/images/639331e65dd407f789da10853f1ea3514f380fb3.jpg";
 import newsImg6 from "../assets/images/39620cb375f2dc62f7cd8df181b09a59d0264d67.jpg";
 
 let popularNews = [
     {
         news_id: 1,
         news_img: newsImg1,
-        news_title: "Kako je Ana Bekuta završila kao manekenka u spotu svog kolege?",
+        news_title: "NASTAVALJAMO U DOBROM RITMU: U SPECIJAL stižu legende naše muzike (VIDEO)",
         news_date: "03.09.2021."
     },
     {
@@ -21,19 +21,19 @@ let popularNews = [
     {
         news_id: 3,
         news_img: newsImg3,
-        news_title: "ŽESTOKA BORBA: 10 takmičara slavilo prolazak, 8 ispalo iz takmičenja",
+        news_title: "„Ne valja da zovemo BOGA SAMO KADA NAM NEŠTO TREBA!“ Marija Šerifović poslala snažnu poruku u svom vlogu (VIDEO)",
         news_date: "03.09.2021."
     },
     {
         news_id: 4,
         news_img: newsImg4,
-        news_title: "„Ne valja da zovemo BOGA SAMO KADA NAM NEŠTO TREBA!“ Marija Šerifović poslala snažnu poruku u svom vlogu (VIDEO)",
+        news_title: "ŽESTOKA BORBA: 10 takmičara slavilo prolazak, 8 ispalo iz takmičenja",
         news_date: "03.09.2021."
     },
     {
         news_id: 5,
         news_img: newsImg5,
-        news_title: "NASTAVALJAMO U DOBROM RITMU: U SPECIJAL stižu legende naše muzike (VIDEO)",
+        news_title: "Kako je Ana Bekuta završila kao manekenka u spotu svog kolege?",
         news_date: "03.09.2021."
     },
     {
@@ -44,7 +44,7 @@ let popularNews = [
     },
     {
         news_id: 7,
-        news_img: newsImg1,
+        news_img: newsImg5,
         news_title: "Kako je Ana Bekuta završila kao manekenka u spotu svog kolege?",
         news_date: "03.09.2021."
     },
@@ -68,7 +68,7 @@ let popularNews = [
     },
     {
         news_id: 11,
-        news_img: newsImg5,
+        news_img: newsImg1,
         news_title: "NASTAVALJAMO U DOBROM RITMU: U SPECIJAL stižu legende naše muzike (VIDEO)",
         news_date: "03.09.2021."
     },
@@ -80,7 +80,7 @@ let popularNews = [
     },
     {
         news_id: 13,
-        news_img: newsImg1,
+        news_img: newsImg5,
         news_title: "Kako je Ana Bekuta završila kao manekenka u spotu svog kolege?",
         news_date: "03.09.2021."
     },
@@ -104,7 +104,7 @@ let popularNews = [
     },
     {
         news_id: 17,
-        news_img: newsImg5,
+        news_img: newsImg1,
         news_title: "NASTAVALJAMO U DOBROM RITMU: U SPECIJAL stižu legende naše muzike (VIDEO)",
         news_date: "03.09.2021."
     },
